f4a68230c65ed91c2ad1918b9999a71ef96a42d2
Laibah-Shahid <laibahshahid18@gmail.com>
2025-04-20 11:34:12 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,16 @@ import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
       <Hero />
       <Features />
       <Contact />
       <Footer />
-    </div>
+    </>
   );
 }
 
