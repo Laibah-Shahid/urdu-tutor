ef1f0c3998c4b53825febd8ea566c319b1dc34e6
Laibah-Shahid <laibahshahid18@gmail.com>
2025-04-20 15:06:00 +0500

    fixed height

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-20 bg-indigo-100">
-      <h2 className="text-3xl font-bold text-center text-indigo-800 mb-6">Contact Us</h2>
+    <section id="contact" className=" bg-indigo-100 w-full h-screen pt-16">
+      <h2 className="text-3xl font-bold text-center text-indigo-800 mb-6 ">Contact Us</h2>
       <form className="max-w-xl mx-auto bg-white p-8 rounded shadow text-gray-800">
         <input type="text" placeholder="Your Name" className="w-full mb-4 p-3 border rounded bg-gray-100" />
         <input type="email" placeholder="Your Email" className="w-full mb-4 p-3 border rounded bg-gray-100" />
